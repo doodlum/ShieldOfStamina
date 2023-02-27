4e986ab1bc1e79cabc6aa490eca9008e823b92c7
Tim <15017472+doodlum@users.noreply.github.com>
2023-02-27 17:53:42 +0000

    Added missing hooks

--- src/hooks.h
+++ src/hooks.h
@@ -3,10 +3,10 @@ class hitEventHook //props to Loki
 {
 public:
     static void InstallHook() {
-        REL::Relocation<uintptr_t> hook{ REL::ID(37673) };
+        REL::Relocation<uintptr_t> hook{ REL::RelocationID(37673, 38627) };
         SKSE::AllocTrampoline(1 << 4);
         auto& trampoline = SKSE::GetTrampoline();
-        _ProcessHit = trampoline.write_call<5>(hook.address() + 0x3C0, processHit);
+        _ProcessHit = trampoline.write_call<5>(hook.address() + REL::Relocate(0x3C0, 0x4A8), processHit);
         logger::info("hit event hook installed!");
     };
 private:
@@ -18,10 +18,10 @@ class staminaRegenHook //props to Ersh
 {
 public:
     static void InstallHook() {
-        REL::Relocation<uintptr_t> hook{ REL::ID(37510) };  // 620690 - a function that regenerates stamina
+        REL::Relocation<uintptr_t> hook{ REL::RelocationID(37510, 38452) };  // 620690 - a function that regenerates stamina
         SKSE::AllocTrampoline(1 << 4);
         auto& trampoline = SKSE::GetTrampoline();
-        _shouldRegenStamina = trampoline.write_call<5>(hook.address() + 0x62, shouldRegenStamina);
+        _shouldRegenStamina = trampoline.write_call<5>(hook.address() + REL::Relocate(0x62, 0x6F), shouldRegenStamina);
         logger::info("stamina regen hook installed");
     };
 private:
